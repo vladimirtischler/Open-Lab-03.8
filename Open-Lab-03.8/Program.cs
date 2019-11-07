3566e08dc75577b33753f2b0ed27f1b13c46e749
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-07 20:44:06 +0100

    gfc

---
+++ Open-Lab-03.8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Open_Lab_03_8
+{
+    class Program
+    {
+        public static bool IsPlural(string word)
+        {
+            return word.EndsWith("s");
+        }
+
+
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            Console.WriteLine(IsPlural(a));
+        }
+    }
+}
